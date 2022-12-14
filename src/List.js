84a1d33d7e29b26d4b9fc4e9f18e9526f742e1ea
ASUS <Yehor Kholodiuk>
2022-12-14 19:09:01 +0200

    initial commit

---
+++ src/List.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+
+const List = () => {
+    const [todos, setTodos] = useState([]);
+    const [page, setPage] = useState(1);
+    const limit = 20;
+    const fetchTodos = (page,limit) => {
+        fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }
+
+    useEffect (() => {
+        fetchTodos(page,limit)
+    }, [])
+    return (
+        <div>
+            {todos.map(todo =>
+                <div key = {todo.id }style = {{padding:30, border:'2px solid black'}}>{todo.id}.{todo.title}</div>)}
+        </div>
+    );
+};
+
+export default List;
